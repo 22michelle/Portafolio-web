5e41af8effc11e405740b27e26434c910c7db4f9
michelle <91968024+22michelle@users.noreply.github.com>
2023-02-28 14:47:19 -0500

    Portafolio actualizado

---
+++ Portafolio Michelle/src/components/Newsletter.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+           <div className="col-12">
+            <h2 className="text-center">Gracias por visitar mi portafolio.</h2>
+           </div>
+          </Row>
+        </div>
+      </Col>
+  )
+}
